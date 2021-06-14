ef808b4a598505fb5c0c40dce98f960954c2d31b
alya <alyaggomaa@gmail.com>
2021-06-14 16:09:47 +0200

    Confirm that module is done processing before quitting

--- modules/RDNS/RDNS.py
+++ modules/RDNS/RDNS.py
@@ -73,12 +73,14 @@ class Module(Module, multiprocessing.Process):
         self.outputqueue.put(vd_text + '|' + self.name + '|[' + self.name + '] ' + str(text))
 
     def run(self):
-        try:
-            # Main loop function
-            while True:
+        # Main loop function
+        while True:
+            try:
                 message = self.c1.get_message(timeout=self.timeout)
                 # Check that the message is for you. Probably unnecessary...
                 if message['data'] == 'stop_process':
+                    # confirm that the module is done processing
+                    __database__.publish('finished_modules', self.name)
                     return True
                 if (message and message['channel'] == 'new_ip'
                             and message['type'] == "message"
@@ -100,11 +102,12 @@ class Module(Module, multiprocessing.Process):
                         continue
                     # Store in the db
                     __database__.setInfoForIPs(ip, data)
-        except KeyboardInterrupt:
-            return True
-        except Exception as inst:
-            self.print('Problem on the run()', 0, 1)
-            self.print(str(type(inst)), 0, 1)
-            self.print(str(inst.args), 0, 1)
-            self.print(str(inst), 0, 1)
-            return True
+            except KeyboardInterrupt:
+                # On KeyboardInterrupt, slips.py sends a stop_process msg to all modules, so continue to receive it
+                continue
+            except Exception as inst:
+                self.print('Problem on the run()', 0, 1)
+                self.print(str(type(inst)), 0, 1)
+                self.print(str(inst.args), 0, 1)
+                self.print(str(inst), 0, 1)
+                return True
